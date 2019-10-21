0200141578ce1adae004625661751a2271a15d56
Sourabh1924 <memo.sourabh@gmail.com>
2019-10-21 20:22:43 +0530

    Add

--- SalesForce/src/com/SaleForce/libraries/Excel_Libraries.java
+++ SalesForce/src/com/SaleForce/libraries/Excel_Libraries.java
@@ -1,9 +1,118 @@
 package com.SaleForce.libraries;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
 public class Excel_Libraries {
 
-	public Excel_Libraries() {
-		// TODO Auto-generated constructor stub
+	public static String Folder_path = Utility_Libraries.Folder_path;
+	static String Excel_path =  Utility_Libraries.Excel_path;
+	static Workbook WB;
+	
+	//-------------------------------------------Excel reporter---------------------------------------------
+	 public static void Excel_Reporter(String Step_details,String Actual,String Status,String Time) throws Throwable
+			{ 
+				String Attribute[] = {"Step_details","Actual","Status","Time"};
+					try{
+						FileInputStream fin=new FileInputStream(Excel_path); 
+						WB=WorkbookFactory.create(fin);
+						}
+					catch(Exception e){
+						System.out.println(e);
+						}
+				Sheet sh = WB.getSheet("Sheet1");
+				int Row_cnt = sh.getLastRowNum();
+				Row newRow = sh.createRow(0);
+				  	for(int j=0;j<=3;j++)
+				  	{		  	
+						newRow.createCell(j).setCellValue(Attribute[j]);
+				  	}
+				  
+				String Attribute_value[] =  {Step_details,Actual,Status,Time};
+				Row newRow1 = sh.createRow(Row_cnt+1);
+					for(int i=0;i<=3;i++)
+						{
+						newRow1.createCell(i).setCellValue(Attribute_value[i]);
+						}	
+				FileOutputStream fout = new FileOutputStream(Excel_path);
+				WB.write(fout);
+				WB.close();
+			}
+		    
+	//------------------------------------------Create Excel file------------------------------------------
+	 public static String Create_Excel_file() throws Throwable
+			{
+				Workbook Wb = null;
+				if ((new File(Excel_path)).exists()==false) {	
+			      		
+					if (Excel_path.endsWith(".xls"))  {  
+							Wb = new HSSFWorkbook();
+						}
+						else  {
+							Wb = new XSSFWorkbook();
+						}
+					Wb.createSheet("Sheet1").createRow(0).createCell(0);
+					FileOutputStream fout = new FileOutputStream(Excel_path);
+					Wb.write(fout);
+					Wb.close();
+				}
+				return Excel_path;
+			}
+
+	 //------------------------------------------For data access--------------------------------------------
+	 public static String Read(String Attri_name,String File_name,String Sheet_name) throws Throwable
+	     {
+			FileInputStream fin=new FileInputStream(System.getProperty("user.dir")+"\\src\\RA_Data\\"+File_name);
+
+			WB=WorkbookFactory.create(fin);
+			Sheet sh = WB.getSheet(Sheet_name);
+			int Row_cnt = sh.getLastRowNum();	
+			   for(int i=0;i<=Row_cnt;i++)  
+			   {
+				   Row rw_obj = sh.getRow(i);
+				   Cell cell_obj = rw_obj.getCell(0);
+				   String Attri = cell_obj.toString();
+				      if(Attri.equals(Attri_name)) 
+				      {
+						   Cell Attri_valu= rw_obj.getCell(1);
+						   return Attri_valu.toString();
+				      }
+			   }
+			return null;		  
+		}
+	 
+	 public static String[][] Read_Data_itera(String File_path,String Sheet_name) throws Throwable
+     {
+		String[][] tabArray = null;
+		FileInputStream fin=new FileInputStream(File_path);
+		WB=WorkbookFactory.create(fin);
+		Sheet sh = WB.getSheet(Sheet_name);
+		int totalrow = sh.getLastRowNum();
+		int totalcol = sh.getRow(0).getPhysicalNumberOfCells();
+		int row = 0;			
+		tabArray = new String[totalrow][totalcol];
+			 for(int i=1;i<=totalrow;i++)  
+			   {
+				 int col = 0;
+					 for(int j=0;j<=totalcol-1;j++)  
+					   {
+						 Cell cell_obj = sh.getRow(i).getCell(j);
+						 tabArray[row][col] = cell_obj.getStringCellValue();
+						 col++;
+					   } 
+				 row++;
+			   }
+		return tabArray;	  		  
 	}
 
 }
--- SalesForce/src/com/SaleForce/libraries/Utility_Libraries.java
+++ SalesForce/src/com/SaleForce/libraries/Utility_Libraries.java
@@ -1,9 +1,137 @@
 package com.SaleForce.libraries;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class Utility_Libraries {
 
-	public Utility_Libraries() {
-		// TODO Auto-generated constructor stub
+	public static WebDriver driver;
+	static String Folder_path = System.getProperty("user.dir")+"\\src\\Test_Result\\Report"+Utility_Libraries.Time_stamp();
+	static String Excel_path = Folder_path+"\\Excelreport"+Utility_Libraries.Time_stamp()+".xls";
+
+  public static WebDriver getBrowser(String strBrowserName)
+	{
+		switch(strBrowserName.toLowerCase())
+		{	
+		//launch in firefox
+		case "firefox":
+		//System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\src\\Drivers\\geckodrivers.exe");
+		driver=new FirefoxDriver();
+		break;
+		//------------------------------------------------
+		//launch in chrome
+		case "chrome":
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\Drivers\\chromedriver.exe");
+		driver=new ChromeDriver();
+		break;
+		//------------------------------------------------		
+		//launch in internetexplore
+		case "internetexplore":
+		System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"\\src\\Drivers\\IEDriverServer.exe");
+		driver=new InternetExplorerDriver();
+		
+		case "ie":
+			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"\\src\\Drivers\\IEDriverServer.exe");
+			driver=new InternetExplorerDriver();
+		//------------------------------------------------
+		default:
+			System.out.println("Driver is not found "+strBrowserName);
+		}
+		return driver;
+	}
+	
+	public static ExtentReports Report()
+	{	
+	 	ExtentReports report1 = new ExtentReports(Folder_path+"\\Test"+Utility_Libraries.Time_stamp()+".html");
+		report1.addSystemInfo("Java version", "JDK 8");
+		report1.addSystemInfo("Machine", "SourabhLaptop");
+		report1.config().documentTitle("Sourabh");
+		report1.config().reportHeadline("Selenium");
+		report1.config().reportName("Automation");
+		return report1;
+	}
+	
+   public static String Time_stamp()
+	{
+		Date now = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy-hh-mm-ss");
+		String time = dateFormat.format(now);
+		return time;
+	}
+	    
+   public static String Screen_Report(WebDriver driver) throws Throwable
+	{
+    	File source_image = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String Image_path = Folder_path+"\\Screeshot"+Utility_Libraries.Time_stamp()+".png";
+		File Desti_image = new File(Image_path);
+		FileUtils.copyFile(source_image,Desti_image);
+		return ""+Desti_image;
+	}
+
+   public static String GetElement(String Keys,String FileName) throws Throwable
+	{
+		FileInputStream pagobj =null;
+		Properties obj = new Properties();
+		try
+		{ 
+			pagobj = new FileInputStream (System.getProperty("user.dir")+"\\src\\com\\FK\\properties\\" + FileName + ".properties");	
+		}
+		catch (Exception e) 
+		{	
+			System.out.println("File is not found");
+		}
+			obj.load(pagobj);
+			return obj.getProperty(Keys);
+	}
+   
+   public static String[] Verify_value(String[] Array,ExtentTest logger)
+   {
+	   for(int i=0;i<=Array.length-1;i++)
+	   {
+		   Assert.assertNotNull(Array[i].toString(), "Value is present");
+		   logger.log(LogStatus.PASS,Array[i].toString(), "Value is present");
+	   }
+	   return Array;
+   }
+   public void Report_pass(String Step_details,String Actual,ExtentTest logger) throws Throwable
+	{
+		  //===========================Reporter======================================================================================
+		  //=========================================================================================================================
+	      logger.log(LogStatus.PASS,Step_details,Actual);
+	      logger.log(LogStatus.PASS,logger.addScreenCapture(Utility_Libraries.Screen_Report(driver)));
+	      Excel_Libraries.Excel_Reporter(Step_details,Actual, "PASS",""+new SimpleDateFormat("MM_dd_y_hhmmssa").format(new Date(0)));
+		  //=========================================================================================================================
+		  //=========================================================================================================================
+	}
+	
+	public void Report_fail(String Step_details,String Actual,ExtentTest logger,WebDriver driver,ExtentReports Extndreport) throws Throwable
+	{
+		  //===========================Reporter=====================================================================================
+		  //========================================================================================================================
+	      logger.log(LogStatus.FAIL,Step_details,Actual);
+	      logger.log(LogStatus.FAIL,logger.addScreenCapture(Utility_Libraries.Screen_Report(driver)));
+	      Excel_Libraries.Excel_Reporter(Step_details,Actual, "FAIL",""+new SimpleDateFormat("MM_dd_y_hhmmssa").format(new Date(0)));
+	      Extndreport.endTest(logger);
+	      Extndreport.flush();
+	      driver.close();
+	      System.exit(0);
+		  //========================================================================================================================
+		  //========================================================================================================================
 	}
 
 }
