72677bfb47439e9dfec88bb21f9051b7b0a8f750
iDeliver20 <iDeliver20@iDeliver20-PC>
2019-10-23 20:28:37 +0530

    add

--- SalesForce/src/com/SaleForce/e2e/RunTest.java
+++ SalesForce/src/com/SaleForce/e2e/RunTest.java
@@ -23,7 +23,8 @@ public class RunTest {
 		driver = new ChromeDriver();
 		driver.navigate().to("https://login.salesforce.com/");
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("rpatestuser31@yopmail.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("rpatestuser32@yopmail.com");
 		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Cloud_123");
 		driver.findElement(By.xpath(" //input[@id='Login']")).click();
 		
@@ -31,17 +32,27 @@ public class RunTest {
         System.out.println("Test1 number of elements: " + elements.size()); */
         driver.findElement(By.xpath("//li[@id='Contract_Tab']//a[contains(text(),'Contracts')]")).click();
         driver.findElement(By.xpath("//input[@name='new']")).click();
-        driver.findElement(By.xpath("//input[@id='ctrc7']")).sendKeys("Arti Sahu");
-        driver.findElement(By.xpath("//input[@id='ctrc16']")).sendKeys("ABCCompany");
+        
+        driver.findElement(By.xpath("//input[@id='ctrc7']")).sendKeys("Amazon");
+        driver.findElement(By.xpath("//input[@id='ctrc16']")).sendKeys("ABC Company");
         driver.findElement(By.xpath("//input[@id='CustomerSignedTitle']")).sendKeys("NewContract");
-        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
         Date dateobj = new Date();
         System.out.println(df.format(dateobj));
 		driver.findElement(By.xpath("//input[@id='ctrc6']")).sendKeys(df.format(dateobj));
 		Select priceBook = new Select(driver.findElement(By.xpath("//select[@id='ctrc17']")));
 		priceBook.selectByVisibleText("Standard");
+		driver.findElement(By.xpath("//input[@id='ctrc5']")).sendKeys(df.format(dateobj));
 		driver.findElement(By.xpath("//input[@id='ctrc40']")).sendKeys("12");
-		//input[@id='ctrc5']
+		driver.findElement(By.xpath("//input[@id='CompanySigned']")).sendKeys("Chris Carpenter");
+		
+		//-------------------------Billing Address----------------------------------------//
+		driver.findElement(By.xpath("//textarea[@id='ctrc25street']")).sendKeys("Uslapur");
+		driver.findElement(By.xpath("//input[@id='ctrc25state']")).sendKeys("Chhattisgarh");
+		driver.findElement(By.xpath("//input[@id='ctrc25country']")).sendKeys("India");
+		driver.findElement(By.xpath("//textarea[@id='Description']")).sendKeys("This is contract");
+		driver.findElement(By.xpath("//div[contains(@class,'pbBottomButtons')]//input[1]")).click();
+		
 		
 		
 			
