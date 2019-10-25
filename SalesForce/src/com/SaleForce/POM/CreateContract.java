510b3fa0d95be92099a2e2c61ca04750d66e464c
iDeliver20 <iDeliver20@iDeliver20-PC>
2019-10-25 18:21:29 +0530

    Add

---
+++ SalesForce/src/com/SaleForce/POM/CreateContract.java
@@ -0,0 +1,15 @@
+package com.SaleForce.POM;
+
+import org.openqa.selenium.By;
+
+import com.SaleForce.libraries.Utility_Libraries;
+
+public class CreateContract {
+
+	public By ContractTab() throws Throwable
+	{
+		By Source = By.xpath(Utility_Libraries.fGetElement("ContractTab", "Contracts"));
+		return Source;
+	}	
+
+}
