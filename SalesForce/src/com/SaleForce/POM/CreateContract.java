c2eaac1e5eec15f95c8c84ea2631f61418912f55
iDeliver20 <iDeliver20@iDeliver20-PC>
2019-10-25 18:23:57 +0530

    Add

--- SalesForce/src/com/SaleForce/POM/CreateContract.java
+++ SalesForce/src/com/SaleForce/POM/CreateContract.java
@@ -11,5 +11,11 @@ public class CreateContract {
 		By Source = By.xpath(Utility_Libraries.fGetElement("ContractTab", "Contracts"));
 		return Source;
 	}	
+	
+	public By NewLink() throws Throwable
+	{
+		By Source = By.xpath(Utility_Libraries.fGetElement("New_Link", "Contracts"));
+		return Source;
+	}
 
 }
