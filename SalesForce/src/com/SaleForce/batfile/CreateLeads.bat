b353368ff5ed3d9c990f06763864ec8ec5f19776
Sourabh1924 <memo.sourabh@gmail.com>
2019-11-12 18:39:21 +0530

    Add

--- SalesForce/src/com/SaleForce/batfile/CreateLeads.bat
+++ SalesForce/src/com/SaleForce/batfile/CreateLeads.bat
@@ -1,4 +1,4 @@
-set projectLocation=refs/remotes/origin/master/SalesForce/SalesForce/
+set projectLocation=SalesForce/SalesForce/
 cd %projectLocation%
 set classpath=%projectLocation%\bin;C:\Java_jar\*
 java org.testng.TestNG %projectLocation%\Leads.xml
