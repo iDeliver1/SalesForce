5abd9579858ee59318925b5b2822dae4530dc11d
Sourabh1924 <memo.sourabh@gmail.com>
2019-11-20 20:37:42 +0530

    add

--- SalesForce/src/com/SaleForce/webelements/SaleForce_AddProduct.java
+++ SalesForce/src/com/SaleForce/webelements/SaleForce_AddProduct.java
@@ -1,11 +1,9 @@
 package com.SaleForce.webelements;
 
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import com.SaleForce.libraries.Utility_Libraries;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
@@ -26,15 +24,30 @@ public class SaleForce_AddProduct {
 
 	public void Product()
 	{
-		driver.findElement(By.xpath("//input[@name='addProd']")).click();
-		
-		List<WebElement> list_  = driver.findElements(By.xpath("//input[@name='addProd']"));
-		for(int i=1;i<=list_.size();i++)
+		try
 		{
-			if(list_.get(i).getText().compareTo(""))
-			{
-				
-			}
+			driver.findElement(By.xpath("//input[@name='addProd']")).click();
+			List<WebElement> Productlist_  = driver.findElements(By.xpath("//div[contains(@id,'PRODUCT_NAME')]"));
+				for(int i=1;i<=Productlist_.size();i++)
+				{
+					if(Productlist_.get(i).getText().contains(""))
+					{
+						List<WebElement> ProductChecklist_  = driver.findElements(By.xpath("//input[contains(@class,'checkbox')]"));
+						ProductChecklist_.get(i).click();
+						break;
+					}
+				}
+				driver.findElement(By.xpath("//input[@name='edit']")).click();
+				driver.findElement(By.xpath("//input[contains(@id,'Quantity')]")).sendKeys("1");
+				driver.findElement(By.xpath("//table[contains(@class,'genericTable ')]//tr[1]//descendant::input[@title='Save']")).click();
+				driver.findElement(By.xpath("//input[@title='Edit All']")).isDisplayed();
 		}
+		catch(Exception e)
+		{
+			
+		}
+			   
+			
+			
 	}
 }
