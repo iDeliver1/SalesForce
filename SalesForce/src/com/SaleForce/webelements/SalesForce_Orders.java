0be5be2d11dd048b59ca3bb79fa6a419034c61c9
iDelive1 <sourabhc@ideliver-inc.com>
2019-11-21 16:49:47 +0530

    add

--- SalesForce/src/com/SaleForce/webelements/SalesForce_Orders.java
+++ SalesForce/src/com/SaleForce/webelements/SalesForce_Orders.java
@@ -1,6 +1,9 @@
 package com.SaleForce.webelements;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.SaleForce.POM.CreateOrder;
 import com.SaleForce.libraries.Utility_Libraries;
 import com.relevantcodes.extentreports.ExtentReports;
@@ -23,55 +26,62 @@ public class SalesForce_Orders {
 		
 		public void Create_Orders(String AccountName,String OrderStartDate,String ContractNumber,String OrderDescription) throws Throwable
 		{
-				String CurrentDate = UtilityObject.fGetCurrentDate();
-				driver.findElement(CreateOrderPOM.OrderTab()).click();
-				Thread.sleep(3000);
 				try
 				{
-					//Close the popup window
-					driver.findElement(CreateOrderPOM.PopUpWindow()).click();
+					WebDriverWait wait = new WebDriverWait(driver,30);
+					String CurrentDate = UtilityObject.fGetCurrentDate();
+					driver.findElement(CreateOrderPOM.OrderTab()).click();
+						try
+						{
+							wait.until(ExpectedConditions.visibilityOfElementLocated(CreateOrderPOM.PopUpWindow()));
+							//Close the popup window
+							driver.findElement(CreateOrderPOM.PopUpWindow()).click();
+						}
+						catch(Exception f) {}
+				 	driver.findElement(CreateOrderPOM.NewLink()).click();
+					 	try
+					 	{
+					 		driver.findElement(CreateOrderPOM.AccountName()).isDisplayed();
+							//-----------------------------Reporter
+					 		UtilityObject.fReportpass("Create Order", "Create Order page is open successfully", logger, driver);
+							//------------------------------------
+					 	}
+					 	catch(Exception e)
+					 	{
+					 		//-----------------------------Reporter
+					 		UtilityObject.fReportfail("Create Order", "Error :" + e +" Create Order page is not open successfully", logger, driver, Extndreport);
+							//------------------------------------
+					 	}
+					wait.until(ExpectedConditions.visibilityOfElementLocated(CreateOrderPOM.AccountName()));
+			        driver.findElement(CreateOrderPOM.AccountName()).sendKeys(AccountName);
+			        driver.findElement(CreateOrderPOM.OrderStartDate()).sendKeys(CurrentDate);
+					driver.findElement(CreateOrderPOM.ContractNumber()).sendKeys(ContractNumber);
+					driver.findElement(CreateOrderPOM.OrderDescription()).sendKeys(OrderDescription);
+					//-------------------------Billing Address----------------------------------------/
+					Thread.sleep(2000);
+					driver.findElement(CreateOrderPOM.OrderSaveButton()).click();
+					
+					//----------------------Verification Contract is Created-------------------------/
+					String OrderNumber_ =  driver.findElement(CreateOrderPOM.OrderNumber()).getText();
+					String OrderNumberCreate = OrderNumber_.replace("Order ","");
+						if(OrderNumberCreate.matches("[0-9]{8}"))
+						{
+						    //-----------------------------Reporter------------------------------------------/
+							UtilityObject.fReportpass("Order create", "Order is successfully created", logger, driver);
+							//-------------------------------------------------------------------------------
+						}
+						else
+						{
+							//-----------------------------Reporter------------------------------------------/
+							UtilityObject.fReportfail("Order create", "Order is not successfully created", logger, driver, Extndreport);
+							//-------------------------------------------------------------------------------/
+						}
 				}
-				catch(Exception f) {}
-				Thread.sleep(2000);
-			 	driver.findElement(CreateOrderPOM.NewLink()).click();
-				 	try
-				 	{
-				 		driver.findElement(CreateOrderPOM.AccountName()).isDisplayed();
-						//-----------------------------Reporter
-				 		UtilityObject.fReportpass("Create Order", "Create Order page is open successfully", logger, driver);
-						//------------------------------------
-				 	}
-				 	catch(Exception e)
-				 	{
-				 		//-----------------------------Reporter
-				 		UtilityObject.fReportfail("Create Order", "Error :" + e +" Create Order page is not open successfully", logger, driver, Extndreport);
-						//------------------------------------
-				 	}
-				 	
-		        driver.findElement(CreateOrderPOM.AccountName()).sendKeys(AccountName);
-		        driver.findElement(CreateOrderPOM.OrderStartDate()).sendKeys(CurrentDate);
-				driver.findElement(CreateOrderPOM.ContractNumber()).sendKeys(ContractNumber);
-				driver.findElement(CreateOrderPOM.OrderDescription()).sendKeys(OrderDescription);
-				//-------------------------Billing Address----------------------------------------/
-				Thread.sleep(2000);
-				driver.findElement(CreateOrderPOM.OrderSaveButton()).click();
-				
-				//----------------------Verification Contract is Created-------------------------/
-				
-				String OrderNumber_ =  driver.findElement(CreateOrderPOM.OrderNumber()).getText();
-				String OrderNumberCreate = OrderNumber_.replace("Order ","");
-					if(OrderNumberCreate.matches("[0-9]{8}")){
-				    //-----------------------------Reporter------------------------------------------/
-					UtilityObject.fReportpass("Order create", "Order is successfully created", logger, driver);
-					//-------------------------------------------------------------------------------
-					}else{
+				catch(Exception e)
+				{
 					//-----------------------------Reporter------------------------------------------/
-					UtilityObject.fReportfail("Order create", "Order is not successfully created", logger, driver, Extndreport);
+					UtilityObject.fReportfail("Error", "Error : " + e, logger, driver, Extndreport);
 					//-------------------------------------------------------------------------------/
-					}
+				}
 		}
-
-		
-	
-
 }
