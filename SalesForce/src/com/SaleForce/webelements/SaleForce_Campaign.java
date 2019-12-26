026328de408b0863db6b6067686d2b1c91b94391
Sourabh1924 <memo.sourabh@gmail.com>
2019-12-26 22:29:57 +0530

    add

--- SalesForce/src/com/SaleForce/webelements/SaleForce_Campaign.java
+++ SalesForce/src/com/SaleForce/webelements/SaleForce_Campaign.java
@@ -2,10 +2,9 @@ package com.SaleForce.webelements;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import com.SaleForce.POM.CreateCampaign;
+import com.SaleForce.libraries.Generic_Libraries;
 import com.SaleForce.libraries.Utility_Libraries;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
@@ -17,6 +16,7 @@ public class SaleForce_Campaign {
 	ExtentReports Extndreport;
 	CreateCampaign CreateCampaignPOM = new CreateCampaign();
 	Utility_Libraries Utility_Object = new Utility_Libraries();
+	Generic_Libraries GenericObject = new Generic_Libraries(logger, driver, Extndreport);
 	
 	public SaleForce_Campaign(ExtentTest logger,WebDriver driver, ExtentReports Extndreport) {
 		// TODO Auto-generated constructor stub
@@ -31,59 +31,30 @@ public class SaleForce_Campaign {
 		String CurrentDate = Utility_Object.fGetCurrentDate();
 		String AddDate = Utility_Object.fAddDate(CurrentDate, 10, Condition);
 		WebDriverWait wait = new WebDriverWait(driver,10);
-		
-		try
-		{
-			driver.findElement(CreateCampaignPOM.Tab()).click();
-				try
-				{
-					wait.until(ExpectedConditions.presenceOfElementLocated(CreateCampaignPOM.Close()));
-					//Close the popup window
-					driver.findElement(CreateCampaignPOM.Close()).click();
-				}
-				catch(Exception f) {}
-			driver.findElement(CreateCampaignPOM.New()).click();
-				try
-				{
-					driver.findElement(CreateCampaignPOM.Name()).isDisplayed();
-					//-----------------------------Reporter
-					Utility_Object.fReportpass("Create Campaign", "Create Campaign page is open successfully", logger, driver);
-					//------------------------------------
-				}
-				catch(Exception e)
-				{
-					//-----------------------------Reporter
-					Utility_Object.fReportfail("Create Campaign", "Error :" + e +" Create Campaign page is not open successfully", logger, driver, Extndreport);
-					//------------------------------------
-				}
-			driver.findElement(CreateCampaignPOM.Name()).sendKeys(Name);
-			Select Status = new Select(driver.findElement(CreateCampaignPOM.Status()));
-			Status.selectByValue(Status_);
-			driver.findElement(CreateCampaignPOM.S_Date()).sendKeys(CurrentDate);
-			driver.findElement(CreateCampaignPOM.E_Date()).sendKeys(AddDate);
-			driver.findElement(CreateCampaignPOM.Expected_rev()).sendKeys(Expected_rev);
-			driver.findElement(CreateCampaignPOM.Budget_cost()).sendKeys(Budget_cost);
-			driver.findElement(CreateCampaignPOM.Save()).click();
-			wait.until(ExpectedConditions.presenceOfElementLocated(CreateCampaignPOM.HeadName()));
-			String CampaignName = driver.findElement(CreateCampaignPOM.HeadName()).getText();
-				if(CampaignName.contains(Name))
-				{
-					//-----------------------------Reporter
-					Utility_Object.fReportpass("Campaign create", "Campaign is successfully created", logger, driver);
-					//------------------------------------
-				}
-				else
-				{
-					//-----------------------------Reporter
-					Utility_Object.fReportfail("Campaign create", "Campaign is not successfully created", logger, driver, Extndreport);
-					//------------------------------------
-				}
-		}
-		catch(Exception r)
-		{
-			//-----------------------------Reporter
-			Utility_Object.fReportfail("Error message", "Error : "+ r, logger, driver, Extndreport);
-			//------------------------------------
-		}
+		GenericObject.Click(driver.findElement(CreateCampaignPOM.Tab()));
+		GenericObject.Click(driver.findElement(CreateCampaignPOM.Close()));
+		GenericObject.Click(driver.findElement(CreateCampaignPOM.New()));
+		GenericObject.IsDisplay(driver.findElement(CreateCampaignPOM.Name()),"Create Campaign","Create Campaign page is open successfully");
+		GenericObject.SendKeys(driver.findElement(CreateCampaignPOM.Name()), Name);
+		GenericObject.Select(driver.findElement(CreateCampaignPOM.Status()), "Value", Status_);
+		GenericObject.SendKeys(driver.findElement(CreateCampaignPOM.S_Date()), CurrentDate);
+		GenericObject.SendKeys(driver.findElement(CreateCampaignPOM.E_Date()), AddDate);
+		GenericObject.SendKeys(driver.findElement(CreateCampaignPOM.Expected_rev()), Expected_rev);
+		GenericObject.SendKeys(driver.findElement(CreateCampaignPOM.Budget_cost()), Budget_cost);
+		GenericObject.Click(driver.findElement(CreateCampaignPOM.Save()));
+		wait.until(ExpectedConditions.presenceOfElementLocated(CreateCampaignPOM.HeadName()));
+		boolean Rc = GenericObject.GetText(driver.findElement(CreateCampaignPOM.HeadName()), "contains", Name);
+			if(Rc == true)
+			{
+				//-----------------------------Reporter
+				Utility_Object.fReportpass("Campaign create", "Campaign is successfully created", logger, driver);
+				//------------------------------------
+			}
+			else
+			{
+				//-----------------------------Reporter
+				Utility_Object.fReportfail("Campaign create", "Campaign is not successfully created", logger, driver, Extndreport);
+				//------------------------------------
+			}
 	}
 }
